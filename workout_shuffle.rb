0d58ccb255768c474eaf6bfab3e452c3b21e7f99
Alouise Quiatchon <alouiseq@gmail.com>
2020-11-28 09:34:17 -0800

    add barebone methods

--- workout_shuffle.rb
+++ workout_shuffle.rb
@@ -2,3 +2,21 @@ require 'bundler/inline'
 
 TEST_MODE = ENV.key?("TEST")
 TEST_INPUT_CSV_FILE_PATH = File.join(File.dirname(__FILE__), "")
+
+def test
+  puts "THIS IS A TEST"
+end
+
+def run(exercise = ARGV[0], input_csv_file_path = ARGV[1], output_csv_file_path = ARGV[2] = input_csv_file_path)
+  exit_because_of("EXERCISE") unless exercise
+  exit_because_of("INPUT CSV FILE PATH") unless input_csv_file_path
+
+  puts "SUCCESSFUL"
+end
+
+def exit_because_of(arg_name)
+  puts "Missing #{arg_name}"
+  exit
+end
+
+TEST_MODE ? test : run

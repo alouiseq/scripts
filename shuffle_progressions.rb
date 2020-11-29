f84380ce5097706bcaa45cfbb53de11cd0c396e6
Alouise Quiatchon <alouiseq@gmail.com>
2020-11-29 15:17:58 -0800

    allow user to control the number of exercises

--- shuffle_progressions.rb
+++ shuffle_progressions.rb
@@ -1,7 +1,7 @@
 require 'bundler/inline'
 
 TEST_MODE = ENV.key?("TEST")
-TEST_TECHNIQUE = "test_technique.csv"
+TEST_TECHNIQUE = "test_technique"
 TEST_INPUT_PATH = File.join(File.dirname(__FILE__), "test_dir")
 OUTPUT_CSV_FILE_PATH = File.join(File.dirname(__FILE__), "shuffled_progressions.csv")
 
@@ -9,10 +9,10 @@ def test
   puts "THIS IS A TEST"
   require "tempfile"
   test_output_file = Tempfile.new
-  test_input_file_path_csv = File.join(TEST_INPUT_PATH, TEST_TECHNIQUE)
+  test_input_file_path_csv = "#{File.join(TEST_INPUT_PATH, TEST_TECHNIQUE)}.csv"
   begin
-    run(TEST_TECHNIQUE, TEST_INPUT_PATH)
-    puts "Difference of #{test_input_file_path_csv} and #{test_output_file.path}"
+    run(TEST_INPUT_PATH, TEST_TECHNIQUE)
+    puts "Difference between #{test_input_file_path_csv} AND #{test_output_file.path}:"
     puts `diff #{test_input_file_path_csv} #{test_output_file.path}`
   ensure
     test_output_file.close
@@ -21,16 +21,18 @@ def test
 
 end
 
-def run(technique = ARGV[0], input_path = ARGV[1])
-  exit_because_of("TECHNIQUE") unless technique
+def run(input_path = ARGV[0], technique = ARGV[1], routine_count = Integer(ARGV[2]) || 5)
   exit_because_of("INPUT CSV FILE PATH") unless input_path
+  exit_because_of("TECHNIQUE") unless technique
   require "csv"
 
   technique_csv = "#{technique}.csv"
   input_file_path_csv = File.join(File.dirname(__FILE__), input_path, technique_csv)
   puts "INPUT FILE PATH CSV: #{input_file_path_csv}"
   progressions = CSV.read(input_file_path_csv)
-  shuffled_routine = progressions.shuffle[1..5]
+  puts "ARGV[2]: #{ARGV[2]}"
+  puts "ROUTINE COUNT: #{routine_count}"
+  shuffled_routine = progressions.shuffle[1..routine_count]
   puts "SHUFFLED ROUTINE: #{shuffled_routine}"
 
   CSV.open(OUTPUT_CSV_FILE_PATH, "wb") do |csv|

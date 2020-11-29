093e76c5a07706494e7dd0f7290b71890e15a6cc
Alouise Quiatchon <alouiseq@gmail.com>
2020-11-29 09:04:38 -0800

    update test inputs

--- shuffle_progressions.rb
+++ shuffle_progressions.rb
@@ -1,17 +1,19 @@
 require 'bundler/inline'
 
 TEST_MODE = ENV.key?("TEST")
-TEST_INPUT_CSV_FILE_PATH = File.join(File.dirname(__FILE__), "test_technique.csv")
+TEST_TECHNIQUE = "test_technique.csv"
+TEST_INPUT_PATH = File.join(File.dirname(__FILE__), "test_dir")
 OUTPUT_CSV_FILE_PATH = File.join(File.dirname(__FILE__), "shuffled_progressions.csv")
 
 def test
   puts "THIS IS A TEST"
   require "tempfile"
   test_output_file = Tempfile.new
+  test_input_file_path_csv = File.join(TEST_INPUT_PATH, TEST_TECHNIQUE)
   begin
   #run()
-  puts "Difference of #{TEST_INPUT_CSV_FILE_PATH} and #{test_output_file}"
-  puts `diff #{TEST_INPUT_CSV_FILE_PATH} #{test_output_file}`
+    puts "Difference of #{test_input_file_path_csv} and #{test_output_file.path}"
+    puts `diff #{test_input_file_path_csv} #{test_output_file.path}`
   ensure
     test_output_file.close
     test_output_file.unlink
@@ -19,9 +21,9 @@ def test
 
 end
 
-def run(exercise = ARGV[0], input_csv_file_path = ARGV[1])
+def run(exercise = ARGV[0], input_path = ARGV[1])
   exit_because_of("EXERCISE") unless exercise
-  exit_because_of("INPUT CSV FILE PATH") unless input_csv_file_path
+  exit_because_of("INPUT CSV FILE PATH") unless input_path
   require "csv"
   puts "SUCCESSFUL"
 end
